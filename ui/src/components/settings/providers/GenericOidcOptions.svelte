ac77b0fe1f740240f0dfdf6e68931056179ecd0b
swazynski <100998105+swazynski@users.noreply.github.com>
2022-12-11 16:34:19 +0100

    Create GenericOidcOptions.svelte

---
+++ ui/src/components/settings/providers/GenericOidcOptions.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Field from "@/components/base/Field.svelte";
+    export let key = "";
+    export let config = {};
+</script>
+
+<div class="section-title">OpenID Connect Endpoints</div>
+<div class="grid">
+    <div class="col-lg-12">
+        <Field class="form-field" name="{key}.authUrl" let:uniqueId required>
+            <label for={uniqueId}>Auth URL</label>
+            <input type="url" id={uniqueId} bind:value={config.authUrl} />
+        </Field>
+    </div>
+    <div class="col-lg-12">
+        <Field class="form-field" name="{key}.tokenUrl" let:uniqueId required>
+            <label for={uniqueId}>Token URL</label>
+            <input type="url" id={uniqueId} bind:value={config.tokenUrl} />
+        </Field>
+    </div>
+    <div class="col-lg-12">
+        <Field class="form-field" name="{key}.userinfoUrl" let:uniqueId required>
+            <label for={uniqueId}>Userinfo URL</label>
+            <input type="url" id={uniqueId} bind:value={config.userinfoUrl} />
+        </Field>
+    </div>
+</div>
