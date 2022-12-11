4c71c11c18132b55e7953d5e035b874bada0b6b2
swazynski <100998105+swazynski@users.noreply.github.com>
2022-12-11 16:27:22 +0100

    Create generic-oidc.go

---
+++ tools/auth/generic-oidc.go
@@ -0,0 +1,63 @@
+package auth
+
+import (
+	"encoding/json"
+
+	"golang.org/x/oauth2"
+)
+
+var _ Provider = (*GenericOidc)(nil)
+
+// NameGenericOidc is the unique name of the generic OIDC provider.
+const NameGenericOidc string = "generic-oidc"
+
+type GenericOidc struct {
+	*baseProvider
+}
+
+// NewGoogleProvider creates new generic OIDC provider instance with some defaults.
+func NewGenericOidcProvider() *GenericOidc {
+	return &Google{&baseProvider{
+		scopes: []string{
+			"profile",
+			"email",
+		},
+		authUrl:    todo,
+		tokenUrl:   todo,
+		userApiUrl: todo,
+	}}
+}
+
+// FetchAuthUser returns an AuthUser instance based on the response from OIDC userinfo endpoint.
+func (p *GenericOidc) FetchAuthUser(token *oauth2.Token) (*AuthUser, error) {
+	data, err := p.FetchRawUserData(token)
+	if err != nil {
+		return nil, err
+	}
+
+	rawUser := map[string]any{}
+	if err := json.Unmarshal(data, &rawUser); err != nil {
+		return nil, err
+	}
+
+	extracted := struct {
+		Id      string
+		Name    string
+		Email   string
+		Picture string
+	}{}
+	if err := json.Unmarshal(data, &extracted); err != nil {
+		return nil, err
+	}
+
+	user := &AuthUser{
+		Id:          extracted.Id,
+		Name:        extracted.Name,
+		Email:       extracted.Email,
+		AvatarUrl:   extracted.Picture,
+		RawUser:     rawUser,
+		AccessToken: token.AccessToken,
+	}
+
+	return user, nil
+}
